652740a297aa6c8abcc359ca91bb9db032bfd160
FloCodin <137909251+FloCodin@users.noreply.github.com>
2024-10-23 14:28:56 +0200

    feat:  create roles+users page

---
+++ src/app/roles/page.tsx
@@ -0,0 +1,11 @@
+type PageProps = {
+
+};
+
+export default function Page(props: PageProps) {
+return (
+  <>
+  <h1 className="flex justify-center text-8xl"> still in Development !!! </h1>
+  </>
+);
+}
---
+++ src/app/users/page.tsx
@@ -0,0 +1,14 @@
+import CreateUser from "@/app/components/users/createUser";
+
+
+type PageProps = {
+
+};
+
+export default function Page(props: PageProps) {
+return (
+  <div>
+  <CreateUser/>
+  </div>
+);
+}
