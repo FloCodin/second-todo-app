adef949a6cb292ccc2a029b5b23556efaf6a05b7
FloCodin <137909251+FloCodin@users.noreply.github.com>
2024-11-27 09:11:29 +0100

    fix: fixed setUserRoles to be a string 100%

--- src/app/components/users/CreateUser.tsx
+++ src/app/components/users/CreateUser.tsx
@@ -31,6 +31,7 @@ export default function CreateUser() {
                 ...prev,
                 [newUser?.id || ""]: [selectedRole]
             }));
+
             setNewUserName("");
             setNewUserEmail("");
             setSelectedRole("");
